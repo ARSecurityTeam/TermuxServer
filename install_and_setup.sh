ec65beaa5e14a99e5ab984e5ac563458f1b1db89
Romeo <67787024+ARSecurityTeam@users.noreply.github.com>
2023-02-08 22:35:48 +0600

    Update install_and_setup.sh

--- install_and_setup.sh
+++ install_and_setup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # Function to install PHP and Apache
 install_php_and_apache() {
@@ -12,46 +12,24 @@ start_apache() {
   apachectl
 }
 
-# Function to install PHPMyAdmin
-install_phpmyadmin() {
-  echo "Installing PHPMyAdmin..."
-  pkg install phpmyadmin
+# Function to create a public directory for Apache
+create_public_directory() {
+  echo "Creating a public directory for Apache..."
+  mkdir ~/www
 }
 
-# Function to create symbolic link for PHPMyAdmin
-create_symbolic_link() {
-  echo "Creating symbolic link for PHPMyAdmin..."
-  ln -s /data/data/com.termux/files/usr/share/phpmyadmin /data/data/com.termux/files/usr/share/apache2/default-site/htdocs/phpmyadmin
-}
-
-# Function to install MySQL
-install_mysql() {
-  echo "Installing MySQL..."
-  pkg install mariadb
-}
-
-# Function to start MySQL service
-start_mysql() {
-  echo "Starting MySQL service..."
-  mysql_install_db --user=root
-  mysqld_safe &
-}
-
-# Function to set root password for MySQL
-set_mysql_root_password() {
-  echo "Setting root password for MySQL..."
-  mysqladmin -u root password 'new-password'
+# Function to create a sample PHP file
+create_sample_php_file() {
+  echo "Creating a sample PHP file..."
+  echo "<?php phpinfo(); ?>" > ~/www/index.php
 }
 
 # Main function to run the script
 main() {
   install_php_and_apache
   start_apache
-  install_phpmyadmin
-  create_symbolic_link
-  install_mysql
-  start_mysql
-  set_mysql_root_password
+  create_public_directory
+  create_sample_php_file
   echo "Installation and setup complete!"
 }
 
